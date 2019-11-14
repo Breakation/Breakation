a1be405919ca65f36ccfbc0b19aef34dd5a6bcb9
Bojarrow <49733696+Bojarrow@users.noreply.github.com>
2019-11-14 17:32:37 +0100

    First Setup
    - Added Folders again
    - Installed Photon Plugin, for networking
    - created NetworkController script
    - created NetworkController object

---
+++ Breakation-master/Project_Breakation/Assets/Scripts/SC_NetworkController.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Photon.Pun added
+// Changed namespace to : MonoBehaviourPunCallbacks
+// Photon API : https://doc-api.photonengine.com/en/pun/v2/index.html
+// Photon Documentation : https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro
+
+
+public class SC_NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
