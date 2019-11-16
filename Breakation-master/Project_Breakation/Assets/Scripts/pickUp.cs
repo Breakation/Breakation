870983ca69dee7cbf364f15a7f8ab79d84dfe8be
ShadaMaayouf <45978415+ShadaMaayouf@users.noreply.github.com>
2019-11-16 13:03:42 +0100

    upload pickUp script
    Script für counting and storing picked up objects. communicates with the GameManager script

---
+++ Breakation-master/Project_Breakation/Assets/Scripts/pickUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *this script will tell the Gamemanager how many  Items (aka Gold bars for the demo) the player has picked
+ */
+public class pickUp : MonoBehaviour
+{
+
+    public int valueOfItem;
+    public GameObject pickUpEffekt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")//Spieler ist mit diesem Tag getaggt
+        {
+            FindObjectOfType<GameManager>().addItem(valueOfItem);//find object that has the gamemanager script attached to it
+
+            //Instantiate creates a copy of whatever object we give its refrence to; here the pickUp object
+            Instantiate(pickUpEffekt, transform.position, transform.rotation); //the position and rotation are those of the pickup object attached to the script
+
+            Destroy(gameObject);
+        }
+    }
+}
