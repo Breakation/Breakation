070a797a601f439d9e885653d700ce85800e3525
ShadaMaayouf <45978415+ShadaMaayouf@users.noreply.github.com>
2019-11-16 12:57:46 +0100

    upload script
    Script for managing Interactionm between player and other objects

---
+++ Breakation-master/Project_Breakation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * This script is for:
+ *  - keeping track of the score
+ *  - respawning the player after dying
+     */
+public class GameManager : MonoBehaviour
+{
+
+    public int currentItem; // keep track of the number of picked up items
+    public Text ItemText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addItem(int itemToAdd)//to add picked up item to the list
+    {
+        currentItem += itemToAdd;
+        ItemText.text = "Points: " + currentItem;
+    }
+}
