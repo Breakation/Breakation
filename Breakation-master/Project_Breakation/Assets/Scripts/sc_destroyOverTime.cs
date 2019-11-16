47c4abdef43ff83f4ca36621f8424a5db1fb2d8c
ShadaMaayouf <45978415+ShadaMaayouf@users.noreply.github.com>
2019-11-16 16:40:57 +0100

    more changes

---
+++ Breakation-master/Project_Breakation/Assets/Scripts/sc_destroyOverTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+  this script removes objects from the world after a certain amount of time
+     */
+public class sc_destroyOverTime : MonoBehaviour
+{
+
+    public float lifetime; // we give it a life time a little bit longer than our pick up effect would last!
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+    void Update()
+    {
+        Destroy(gameObject, lifetime);
+        /*lifetime -= Time.deltaTime;
+        if(lifetime < 0)
+        {
+            Destroy(gameObject);
+        }*/
+    }
+}
