ed90bc509058acac403aaa47767e5930bf114dda
Bojarrow <49733696+Bojarrow@users.noreply.github.com>
2019-11-17 15:30:57 +0100

    Enemy + Network
    Enemy Patrol script added
    enemy sight scripts added
    menu prototype scene added
    first network try

---
+++ Breakation-master/Project_Breakation/Assets/Scripts/Patrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+
+        randomSpot = Random.Range(0, moveSpots.Length);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f){
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+
+
+        }
+
+        Vector3 direction = moveSpots[randomSpot].position - transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+
+
+    }
+}
---
+++ Breakation-master/Project_Breakation/Assets/Scripts/sc_photonLobbyCustomMatch.cs
@@ -0,0 +1,97 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_photonLobbyCustomMatch : MonoBehaviourPunCallbacks, ILobbyCallbacks
+{
+
+
+    public static sc_photonLobbyCustomMatch lobby;
+
+    public string roomName;
+    public GameObject roomListingPrefab;
+    public Transform roomsPanel;
+   
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+
+        RemoveRoomListings();
+        foreach (RoomInfo room in roomList)
+        {
+            ListRoom(room);
+        }
+
+    }
+
+    void RemoveRoomListings()
+    {
+        while(roomsPanel.childCount != 0)
+        {
+            Destroy(roomsPanel.GetChild(0).gameObject);
+        }
+    }
+
+    void ListRoom(RoomInfo room)
+    {
+        if(room.IsOpen && room.IsVisible)
+        {
+            GameObject tempListing = Instantiate(roomListingPrefab, roomsPanel);
+            sc_RoomButton tempButton = tempListing.GetComponent<sc_RoomButton>();
+
+            tempButton.roomName = room.Name;
+
+            tempButton.SetRoom();
+
+        }
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Trying to create a new room");
+
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)2 };
+        PhotonNetwork.CreateRoom(roomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be a room with the same name");
+    }
+
+    public void onRoomNameChanged(string nameIn)
+    {
+        roomName = nameIn;
+    }
+
+    public void JoinLobbyOnClick()
+    {
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+}
