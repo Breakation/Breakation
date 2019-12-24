83b2f469d2dce6a847c8ac229fbcaa453ca5a3a3
ShadaMaayouf <45978415+ShadaMaayouf@users.noreply.github.com>
2019-12-24 21:21:48 +0100

    additions to code logical

--- Breakation-master/Project_Breakation/Assets/Scripts/CodeLogical/Keypad.cs
+++ Breakation-master/Project_Breakation/Assets/Scripts/CodeLogical/Keypad.cs
@@ -20,34 +20,49 @@ public class Keypad : MonoBehaviour
         input = "";
     }
 
+    private void Update()
+    {
+        if(input == currentPassword)
+        {
+            doorOpen = true;
+
+        }
+        if (doorOpen)
+        {
+            doorHinge.rotation = Quaternion.RotateTowards(doorHinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 250);
+        }
+    }
     private void OnGUI()
     {
-        if (onTrigger)
+        if (!doorOpen)
         {
-            GUI.Box(new Rect(0, 0, 200, 25), "Press E to open Keypad!");
-
-            if (Input.GetKeyDown(KeyCode.E))
+            if (onTrigger)
             {
-                keypadScreen = true;
-                onTrigger = false;
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E to open Keypad!");
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    keypadScreen = true;
+                    onTrigger = false;
+                }
             }
-        }
 
-        if (keypadScreen)
-        {
-            GUI.Box(new Rect(0, 0, 320, 455), "");
-            GUI.Box(new Rect(5, 5, 310, 25), input); //tempInput
+            if (keypadScreen)
+            {
+                GUI.Box(new Rect(0, 0, 320, 455), "");
+                GUI.Box(new Rect(5, 5, 310, 25), input); //tempInput
 
-            generateScreenOutput(5,35,"1");
-            generateScreenOutput(110,35,"2");
-            generateScreenOutput(215,35,"3");
-            generateScreenOutput(5,140,"4");
-            generateScreenOutput(110,140,"5");
-            generateScreenOutput(215,140,"6");
-            generateScreenOutput(5,245,"7");
-            generateScreenOutput(110,245,"8");
-            generateScreenOutput(215,245,"9");
-            generateScreenOutput(110,350,"0");
+                generateScreenOutput(5, 35, "1");
+                generateScreenOutput(110, 35, "2");
+                generateScreenOutput(215, 35, "3");
+                generateScreenOutput(5, 140, "4");
+                generateScreenOutput(110, 140, "5");
+                generateScreenOutput(215, 140, "6");
+                generateScreenOutput(5, 245, "7");
+                generateScreenOutput(110, 245, "8");
+                generateScreenOutput(215, 245, "9");
+                generateScreenOutput(110, 350, "0");
+            }
         }
 
     }
