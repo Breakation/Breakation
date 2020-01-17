380c9c37351cdc6c3dbcd7af402769a654e805c5
Axel <axki12@web.de>
2020-01-17 17:34:46 +0100

    Map 2.0

---
+++ Breakation-master/Project_Breakation/Assets/Scripts/RoomOnOff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomOnOff : MonoBehaviour
+{
+
+    public GameObject[] room, mini;
+    public static bool roomChanged;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (roomChanged)
+        {
+            for (int i = 0; i < room.Length; i++)
+            {
+                mini[i].gameObject.SetActive(room[i].activeInHierarchy);
+            }
+            roomChanged = false;
+        }
+        
+    }
+}
