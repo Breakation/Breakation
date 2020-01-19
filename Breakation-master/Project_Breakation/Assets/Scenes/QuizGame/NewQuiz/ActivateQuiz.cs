69f6e86292738310254173364b04fa8b9eadc6a0
Leon <leon-stricker@web.de>
2020-01-19 23:40:05 +0100

    Quiz Final push
    Einbau neues Quiz in map. hoffentlich keine Errors bei euch... Bei mir läuft alles einwandfrei

---
+++ Breakation-master/Project_Breakation/Assets/Scenes/QuizGame/NewQuiz/ActivateQuiz.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateQuiz : MonoBehaviour
+{
+    [SerializeField] GameObject QuizGameNew;
+
+    public static bool DeaktivateQuiz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuizGameNew.SetActive(false);
+        DeaktivateQuiz = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            QuizGameNew.SetActive(true);
+        }
+        if (DeaktivateQuiz)
+        {
+            QuizGameNew.SetActive(false);
+            DeaktivateQuiz = false;
+        }
+    }
+}
